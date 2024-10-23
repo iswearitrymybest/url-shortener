0cbe81222623a6eda2d945fcec206eb3bfd1298a
ghostvoid <maximnikitin2002@yandex.ru>
2024-10-23 22:28:56 +0300

    add db

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,16 +1,31 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
 
 	"github.com/ghostvoid/url-shortener/internal/config"
+	sl "github.com/ghostvoid/url-shortener/internal/config/lib/logger/slog"
+	"github.com/ghostvoid/url-shortener/internal/storage/sqlite"
 )
 
 func main() {
-	//cleanevn
+	//cleanevn for config package
+	//slog - logger
 	cfg := config.MustLoad()
 
-	fmt.Println(cfg)
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting url-shortener", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
+
+	storage, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		log.Error("failed to init storage", sl.Err(err))
+		os.Exit(1)
+	}
+
+	_ = storage
 
 	//TODO: init logger
 
@@ -20,4 +35,29 @@ func main() {
 
 	//TODO: run server
 
+}
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+	return log
 }
